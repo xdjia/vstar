cfaed4770b4fc5c51861265626d99a0a2c0cf708
Xiaodong Jia <jiaxiaodong1994@gmail.com>
2024-03-31 17:46:20 -0400

    up

--- setup_glade.sh
+++ setup_glade.sh
@@ -1,6 +1,7 @@
 # copy the binaries to suitable places 
 for grammar in lisp json xml while mathexpr; 
 do 
+    mkdir -p other-artifact/glade/examples/$grammar/;
     cp -f micro-benchmarks/$grammar/cpp-build/file_parser other-artifact/glade/examples/$grammar/parse_$grammar
 done
 
